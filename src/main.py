f238bfb7dce14cda4919bac151f7f94053e5c261
sora7672 <s0ra.bloodydiary@gmail.com>
2024-08-13 12:48:45 +0200

    fill up from visuals

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 
-def start():
+
+
+# # # # Database stuff # # # #
+
+def create_database():
     pass
 
 
+def get_all_entrys():
+    pass
+
+def add_entry(first_name:str, last_name:str, number:str, note:str):
+    pass
+
+def get_entry_by_name(first_name = None, last_name = None):
+    pass
+
+
+def test():
+    pass
+
 if __name__ == '__main__':
-    start()
+    test()
